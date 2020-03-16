401f8bf0cd71ec9c6500dd815f2edba7b1a688bc
Justin Cletus <justinbeckh@gmail.com>
2020-03-16 16:25:31 +0530

    App test url and rest framwork end point urls are added

--- wsptest_share/wsptest_share/urls.py
+++ wsptest_share/wsptest_share/urls.py
@@ -19,13 +19,26 @@ from django.conf.urls import re_path, url
 from django.contrib.auth import views as auth_views
 from login_app import views as login_view
 from app_registration import views as reg_view
+from mtest import views as mtest_view
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'skill_test', mtest_view.SkillViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('^$', mtest_view.index, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('accounts/profile/', login_view.profile, name='profile'),
     path('signup/', reg_view.signup, name='signup'),
     path('sent/', reg_view.activation_sent_view, name="activation_sent"),
     path('activate/<slug:uidb64>/<slug:token>/', reg_view.activate, name="activate"),
+    path('tests/', mtest_view.testList, name="test_list"),
+    re_path(r'^test/(?P<pk>\d+)$', mtest_view.testQuestions, name="test_detail"),
+    re_path(r'^', include(router.urls)),
+    re_path('api-auth/', include('rest_framework.urls', namespace="rest_framework")),
+    path('test_cate/', mtest_view.testCategory, name="testcate"),
+    path('submitted-test/', mtest_view.submittedTest, name="submitted_test"),
+    
 ]
